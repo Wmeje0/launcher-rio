a70ec065756ce5abd872a48967808bd1cfccf252
FaroTechGitHub <140162734+FaroTechGitHub@users.noreply.github.com>
2023-11-21 16:08:52 +0100

    code update
    added NetworkTables support

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -13,21 +13,27 @@
 #include <frc/motorcontrol/MotorControllerGroup.h>
 #include <cameraserver/CameraServer.h>
 #include <iostream>
+
+#include <networktables/DoubleTopic.h>
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
 class Robot : public frc::TimedRobot
 {
 	/**
-	* SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
-	* 
-	* The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
-	* first parameter
-	* 
-	* The motor type is passed as the second parameter. Motor type can either be:
-	*  rev::CANSparkMax::MotorType::kBrushless
-	*  rev::CANSparkMax::MotorType::kBrushed
-	* 
-	* The example below initializes four brushless motors with CAN IDs 1, 2, 3 and 4. Change
-	* these parameters to match your setup
-	*/
+	 * SPARK MAX controllers are intialized over CAN by constructing a CANSparkMax object
+	 *
+	 * The CAN ID, which can be configured using the SPARK MAX Client, is passed as the
+	 * first parameter
+	 *
+	 * The motor type is passed as the second parameter. Motor type can either be:
+	 *  rev::CANSparkMax::MotorType::kBrushless
+	 *  rev::CANSparkMax::MotorType::kBrushed
+	 *
+	 * The example below initializes four brushless motors with CAN IDs 1, 2, 3 and 4. Change
+	 * these parameters to match your setup
+	 */
 	static const int leftFrontDeviceID = 2, leftRearDeviceID = 3, rightFrontDeviceID = 4, rightRearDeviceID = 5;
 	rev::CANSparkMax m_leftFrontMotor{leftFrontDeviceID, rev::CANSparkMax::MotorType::kBrushed};
 	rev::CANSparkMax m_rightFrontMotor{rightFrontDeviceID, rev::CANSparkMax::MotorType::kBrushed};
@@ -35,11 +41,11 @@ class Robot : public frc::TimedRobot
 	rev::CANSparkMax m_rightRearMotor{rightRearDeviceID, rev::CANSparkMax::MotorType::kBrushed};
 
 	/**
-	* In RobotInit() below, we will group left and right motors using the MotorControllerGroup class.
-	*    
-	* For this reason, we only need to pass motor groups to m_robotDrive.
-	* Whatever commands are sent to them will be automatically forwarded to the motors.
-	*/
+	 * In RobotInit() below, we will group left and right motors using the MotorControllerGroup class.
+	 *
+	 * For this reason, we only need to pass motor groups to m_robotDrive.
+	 * Whatever commands are sent to them will be automatically forwarded to the motors.
+	 */
 
 	frc::MotorControllerGroup m_leftMotors{m_leftFrontMotor, m_leftRearMotor};
 	frc::MotorControllerGroup m_rightMotors{m_rightFrontMotor, m_rightRearMotor};
@@ -55,16 +61,34 @@ class Robot : public frc::TimedRobot
 
 	const float dPadSpeed = 0.5;
 	bool squareInputs = false;
-  
- public:
+
+public:
+double x; double y;
 	void RobotInit()
 	{
-		// frc::CameraServer::StartAutomaticCapture();
+		auto inst = nt::NetworkTableInstance::GetDefault();
+		auto table = inst.GetTable("datatable");
+
+		auto xPub = table->GetDoubleTopic("x").Publish();
+		auto yPub = table->GetDoubleTopic("y").Publish();
+
+		xPub.Set(0.69);
+		yPub.Set(4.20);
+
+		// wehrufas8fh8ushfosdfiuhiosudhfioudfsoiigsd
+		
+		auto xSub = table->GetDoubleTopic("x").Subscribe(0.0);
+		auto ySub = table->GetDoubleTopic("y").Subscribe(0.0);
+
+		x = xSub.Get();
+		y = ySub.Get();
+
+
 		/**
-	 	* The RestoreFactoryDefaults method can be used to reset the configuration parameters
-	 	* in the SPARK MAX to their factory default state. If no argument is passed, these
-	 	* parameters will not persist between power cycles
-	 	*/
+		 * The RestoreFactoryDefaults method can be used to reset the configuration parameters
+		 * in the SPARK MAX to their factory default state. If no argument is passed, these
+		 * parameters will not persist between power cycles
+		 */
 		m_leftFrontMotor.RestoreFactoryDefaults();
 		m_rightFrontMotor.RestoreFactoryDefaults();
 		m_leftRearMotor.RestoreFactoryDefaults();
@@ -74,16 +98,17 @@ class Robot : public frc::TimedRobot
 
 		std::cout << "Speed mode set to " << speedMulFactor << ", drive mode set to " << driveMode << "\n";
 		std::cout << "Square inputs set to " << squareInputs << "\n";
-  	}
+	}
 
 	void TeleopPeriodic()
 	{
+		std::cout << x << std::endl;
 		// Drive the robot
 		switch (driveMode)
 		{
 		case 0:
 			m_robotDrive.TankDrive(pad.GetLeftY() * speedMulFactor, pad.GetRightY() * speedMulFactor, squareInputs);
-	  		break;
+			break;
 		case 1:
 			m_robotDrive.ArcadeDrive(pad.GetLeftY() * speedMulFactor, pad.GetLeftX() * speedMulFactor, squareInputs);
 			break;
@@ -91,18 +116,25 @@ class Robot : public frc::TimedRobot
 			m_robotDrive.ArcadeDrive(pad.GetLeftY() * speedMulFactor, pad.GetRightX() * speedMulFactor, squareInputs);
 			break;
 		case 3:
-			if(pad.GetPOV() == 0){
+			if (pad.GetPOV() == 0)
+			{
 				m_robotDrive.ArcadeDrive(dPadSpeed * speedMulFactor * -1, 0, squareInputs);
-			} else if(pad.GetPOV() == 90){
+			}
+			else if (pad.GetPOV() == 90)
+			{
 				m_robotDrive.ArcadeDrive(0, dPadSpeed * speedMulFactor, squareInputs);
-			} else if(pad.GetPOV() == 180){
+			}
+			else if (pad.GetPOV() == 180)
+			{
 				m_robotDrive.ArcadeDrive(dPadSpeed * speedMulFactor, 0, squareInputs);
-			} else if(pad.GetPOV() == 270){
+			}
+			else if (pad.GetPOV() == 270)
+			{
 				m_robotDrive.ArcadeDrive(0, dPadSpeed * speedMulFactor * -1, squareInputs);
 			}
 			break;
 		}
-	
+
 		// If one of the buttons pressed, change the drive mode
 		if (pad.GetAButtonPressed() && driveMode != 0)
 		{
@@ -128,13 +160,13 @@ class Robot : public frc::TimedRobot
 		// If one of the bumpers were pressed, change the speed
 		if (pad.GetRightBumperPressed() && speedMulFactor < 1)
 		{
-		  speedMulFactor += 0.2;
-		  std::cout << "Speed Mode: " << speedMulFactor << "\n";
+			speedMulFactor += 0.2;
+			std::cout << "Speed Mode: " << speedMulFactor << "\n";
 		}
 		if (pad.GetLeftBumperPressed() && speedMulFactor > 0.4)
 		{
-		  speedMulFactor -= 0.2;
-		  std::cout << "Speed Mode: " << speedMulFactor << "\n";
+			speedMulFactor -= 0.2;
+			std::cout << "Speed Mode: " << speedMulFactor << "\n";
 		}
 
 		// If the Back button pressed change the square input
@@ -143,12 +175,26 @@ class Robot : public frc::TimedRobot
 			squareInputs = !squareInputs;
 			std::cout << "Square Inputs: " << squareInputs << "\n";
 		}
-		if(squareInputs && speedMulFactor <= 0.3){
+		if (squareInputs && speedMulFactor <= 0.3)
+		{
 			speedMulFactor = 0.4;
 		}
-  	} 
+	}
+
+	void AutonomousInit()
+	{
+		std::cout << "Autonomous enabled" << std::endl;
+	}
+
+	void AutonomousPeriodic()
+	{
+		std::cout << "Autonomous test" << std::endl;
+	}
 };
 
 #ifndef RUNNING_FRC_TESTS
-int main() { return frc::StartRobot<Robot>(); }
+int main()
+{
+	return frc::StartRobot<Robot>();
+}
 #endif
