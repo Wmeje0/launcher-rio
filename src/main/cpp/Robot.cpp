960513452829c2f08a3662bd541147601a38ad33
Wmeje0 <84284499+Wmeje0@users.noreply.github.com>
2024-02-17 16:14:09 +0100

    added launcher

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -8,6 +8,7 @@
 #include <frc/Joystick.h>
 #include <frc/TimedRobot.h>
 #include <frc/drive/DifferentialDrive.h>
+#include <frc/DigitalInput.h>
 #include "rev/CANSparkMax.h"
 #include <frc/XboxController.h>
 #include <frc/motorcontrol/MotorControllerGroup.h>
@@ -51,6 +52,13 @@ class Robot : public frc::TimedRobot
 	WPI_VictorSPX victorSPX3{3};
 	WPI_VictorSPX victorSPX4{4};
 
+	WPI_TalonSRX launcherLoadTalon{5}; // IDs temporary, controllers do not exist physically yet
+	WPI_TalonSRX launchTalon1{6};
+	WPI_TalonSRX launchTalon2{7};
+
+	frc::DigitalInput ringoInLimit{0}; // IDs also temporary
+	frc::DigitalInput ringoOutLimit{1}; // IDs also temporary
+
 	// Define MotorControllerGroup objects using references
 	frc::MotorControllerGroup m_leftMotors{victorSPX2, victorSPX3};
 	frc::MotorControllerGroup m_rightMotors{victorSPX1, victorSPX4};
@@ -73,6 +81,12 @@ class Robot : public frc::TimedRobot
 	const float dPadSpeed = 0.5;
 	bool squareInputs = false;
 
+	short launcherMode = 0; // 0 - ready for pickup, 1 - loaded, 2 - awaiting ejection confirm, 3 - failiure
+	const float launcherSpeed = 1;
+	const float loaderSpeed = 0.2;
+	short launcherWait = 0;
+	bool launcherWaitStart = false;
+
 public:
 	double x;
 	double y;
@@ -86,6 +100,10 @@ public:
 		victorSPX2.Set(ControlMode::PercentOutput, 0);
 		victorSPX3.Set(ControlMode::PercentOutput, 0);
 		victorSPX4.Set(ControlMode::PercentOutput, 0);
+		
+		launcherLoadTalon.Set(ControlMode::PercentOutput, 0);
+		launchTalon1.Set(ControlMode::PercentOutput, 0);
+		launchTalon2.Set(ControlMode::Follower, 6);
 
 		/**
 		 * The RestoreFactoryDefaults method can be used to reset the configuration parameters
@@ -97,13 +115,19 @@ public:
 		victorSPX3.ConfigFactoryDefault();
 		victorSPX4.ConfigFactoryDefault();
 
+		launcherLoadTalon.ConfigFactoryDefault();
+		launchTalon1.ConfigFactoryDefault();
+		launchTalon2.ConfigFactoryDefault();
+
 		m_rightMotors.SetInverted(true);
+		launchTalon2.SetInverted(InvertType::OpposeMaster);
 	}
 
 	void TeleopInit()
 	{
 		std::cout << "Speed mode set to " << speedMulFactor << ", drive mode set to " << driveMode << "\n";
 		std::cout << "Square inputs set to " << squareInputs << "\n";
+		std::cout << "Launcher on" << "\n";
 	}
 
 	void TeleopPeriodic()
@@ -188,6 +212,8 @@ public:
 		{
 			speedMulFactor = 0.4;
 		}
+
+		operateLauncher();
 	}
 
 	void AutonomousInit()
@@ -220,6 +246,39 @@ public:
 		// } else if (y > x) {
 		// m_robotDrive.TankDrive(speedMulFactor, speedMulFactor, squareInputs);
 		// }
+	}
+
+	void operateLauncher() 
+	{
+		switch (launcherMode) {
+			case 0:
+				if (ringoInLimit.Get()) launcherMode = 1;
+				launcherLoadTalon.Set(ControlMode::PercentOutput, loaderSpeed);
+				launchTalon1.Set(ControlMode::PercentOutput, launcherSpeed);
+			case 1:
+				if (pad.GetLeftTriggerAxis() >= 0.3) // temporary shot input, subject to change
+				{
+					launcherMode = 2;
+					launcherWait = 0;
+					launcherWaitStart = true;
+				} 
+				launcherLoadTalon.Set(ControlMode::PercentOutput, 0);
+				launchTalon1.Set(ControlMode::PercentOutput, launcherSpeed);
+			case 2:
+				if (launcherWait > 5) launcherMode = 3;
+				else if (ringoOutLimit.Get()) 
+				{
+					launcherMode = 0;
+					launcherWait = 0;
+					launcherWaitStart = false;
+				}
+				launcherLoadTalon.Set(ControlMode::PercentOutput, loaderSpeed);
+				launchTalon1.Set(ControlMode::PercentOutput, launcherSpeed);
+			case 3:
+				launcherLoadTalon.Set(ControlMode::PercentOutput, 0);
+				launchTalon1.Set(ControlMode::PercentOutput, 0);
+		}
+		if (launcherWaitStart) launcherWait += 1;
 	}
 };
 
